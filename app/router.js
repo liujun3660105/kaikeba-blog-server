0cce4e987ff8308e3091b0bc0797a14227fd37ac
June <liujun198707@126.com>
2020-03-23 21:28:23 +0800

    chore(router.js): 测试修改

--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
+  console.log(app);
   const { router, controller } = app;
   router.get('/', controller.home.index);
 };
